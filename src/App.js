3e5a008dc5b53e9f87ede2ef624261513eaa2a4a
WumiDev <wumi2021@gmail.com>
2023-04-14 17:20:59 +0000

    modified: App.js & MerchantBackend.json

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import { ethers } from "ethers";
 import { MirrorNodeClient } from "../src/mirrorNodeClient";
 
 // Part 1 - import ABI
+import MerchantBackend from "./MerchantBackend.json";
 
 function App() {
   const [defaultAccount, setDefaultAccount] = useState("");
@@ -25,12 +26,34 @@ function App() {
   const [contract, setContract] = useState();
 
   // Part 2 - define environment variables
+  const scAddress = process.env.REACT_APP_SC_ADDRESS;
+  const nftAddress = process.env.REACT_APP_NFT_ADDRESS;
+  const nftId = AccountId.fromSolidityAddress(nftAddress).toString();
+  const ftAddress = process.env.REACT_APP_FT_ADDRESS;
+  const ftId = AccountId.fromSolidityAddress(ftAddress).toString();
+  const topicId = process.env.REACT_APP_TOPIC_ID;
+  
+  const merchantKey = PrivateKey.fromString(process.env.REACT_APP_MERCHANT_PRIVATE_KEY);
+  const merchantId = AccountId.fromString(process.env.REACT_APP_MERCHANT_ID);
+  const merchantAddress = process.env.REACT_APP_MERCHANT_ADDRESS;
+  
+  const customerKey = PrivateKey.fromString(process.env.REACT_APP_CUSTOMER_KEY);
+  const customerAccount = AccountId.fromString(process.env.REACT_APP_CUSTOMER_ACCOUNT_ID);
 
   // Part 3 - create client instance
+const client = Client.forTestnet().setOperator(merchantId, merchantKey);
 
   const connect = async () => {
     if (window.ethereum) {
       // Part 4 - connect wallet
+      const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      signer.getAddress().then(setDefaultAccount);
+      window.ethereum.on("accountsChanged", changeConnectedAccount);
+
+const contractInstance = new ethers.Contract(scAddress, MerchantBackend.abi, signer);
+setContract(contractInstance);
     }
   };
 
@@ -45,12 +68,26 @@ function App() {
 
   const getContract = async () => {
     // Part 5 - create contract instance
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    const signer = provider.getSigner();
+    signer.getAddress().then(setDefaultAccount);
+    const contractInstance = new ethers.Contract(scAddress, MerchantBackend.abi, signer);
+    setContract(contractInstance);
   };
 
   const getScore = async () => {
     try {
       if (defaultAccount) {
         // Part 17 - get reputation token score
+        await fetch(`https://testnet.mirrornode.hedera.com/api/v1/accounts/${defaultAccount}/tokens?token.id=${ftId}`)
+     .then((response) => response.json())
+     .then((data) => {
+      if (!data.tokens[0]) {
+       setScore(0);
+       return;
+      }
+      setScore(data.tokens[0].balance);
+     });
       }
     } catch (e) {
       console.log(e);
@@ -66,8 +103,22 @@ function App() {
     try {
       if (!contract) getContract();
       // Part 6 - add new car
-
+      const tx = await contract.mintNFT(nftAddress, [Buffer.from(cid)], {
+        gasLimit: 1_000_000,
+       });
+       await tx.wait();
       // Part 7 - submit add new car logs to topic
+      new TopicMessageSubmitTransaction()
+      .setTopicId(topicId)
+      .setMessage(
+      `{
+    type: Minting,
+    accountAddr: ${defaultAccount},
+    tokenId: ${nftId}
+    }`
+      )
+    .execute(client);
+
 
       alert("Successfully added new car!");
     } catch (e) {
@@ -78,24 +129,64 @@ function App() {
 
   const associateNFTToken = async (id) => {
     // Part 8 - associate NFT
+    const associateTransaction = new TokenAssociateTransaction().setAccountId(customerAccount).setTokenIds([id]).freezeWith(client);
+const signedAssociateTransaction = await associateTransaction.sign(customerKey);
+const transactionResponse = await signedAssociateTransaction.execute(client);
+const associateRx = await transactionResponse.getReceipt(client);
+console.log(`associated with NFT status: ${associateRx.status}`);
   };
 
   const associateFTToken = async () => {
     // Part 9 - associate fungible token
+    const associateTransaction = new TokenAssociateTransaction().setAccountId(customerAccount).setTokenIds([ftId]).freezeWith(client);
+const signedAssociateTransaction = await associateTransaction.sign(customerKey);
+const transactionResponse = await signedAssociateTransaction.execute(client);
+const associateRx = await transactionResponse.getReceipt(client);
+console.log(`associated with Fungible Token status: ${associateRx.status}`);
   };
 
   const isAssociated = async (id) => {
     // Part 10 - check token association
+    const mirrorNodeClient = new MirrorNodeClient("testnet");
+return await mirrorNodeClient
+  .getAccountInfo(customerAccount)
+  .then((acc) => {
+  const associatedTokensList = acc.balance.tokens;
+  return associatedTokensList.some((token) => token.token_id === id);
+  })
+  .catch((rejectErr) => {
+  console.log("Could not get token balance", rejectErr);
+  });
   };
 
   const borrowCar = async (id, serial) => {
     // Part 11 - check if tokens are associated, associate them if not
+    if (!(await isAssociated(id))) {
+      await associateNFTToken(id);
+      await associateFTToken();
+    }
 
     try {
       if (!contract) getContract();
       // Part 12 - borrow new car
+      const tx = await contract.borrowing(AccountId.fromString(id).toSolidityAddress(), serial, {
+        value: ethers.utils.parseEther("1000"),
+        gasLimit: 2_000_000,
+       });
+       await tx.wait();
 
       // Part 13 - submit borrow car logs to topic
+      new TopicMessageSubmitTransaction()
+    .setTopicId(topicId)
+    .setMessage(
+     `{
+   type: Borrowing,
+   accountAddr: ${defaultAccount},
+   tokenId: ${id},
+   serial: ${serial}
+  }`
+    )
+    .execute(client);
 
       alert("Successfully borrowed car!");
     } catch (e) {
@@ -122,10 +213,33 @@ function App() {
       if (!contract) getContract();
 
       // Part 14 - give SC allowance
+      client.setOperator(customerAccount, customerKey);
+const smartContractAccountId = await getContractId();
+const allowanceApproveTxResponse = await new AccountAllowanceApproveTransaction()
+.approveTokenNftAllowanceAllSerials(id, customerAccount, smartContractAccountId)
+.freezeWith(client)
+.execute(client);
+client.setOperator(merchantId, merchantKey);
+console.log(`allowance approve tx response: ${allowanceApproveTxResponse}`);
 
       // Part 15 - return car
+      const tx = await contract.returning(AccountId.fromString(id).toSolidityAddress(), serial, {
+        gasLimit: 1_000_000,
+        });
+        await tx.wait();
 
       // Part 16 - submit return car logs to topic
+      new TopicMessageSubmitTransaction()
+    .setTopicId(topicId)
+    .setMessage(
+     `{
+   type: Returning,
+   accountAddr: ${defaultAccount},
+   tokenId: ${id},
+   serial: ${serial}
+  }`
+    )
+    .execute(client);
 
       alert("Successfully returned car!");
     } catch (e) {
@@ -138,8 +252,28 @@ function App() {
     try {
       if (!contract) getContract();
       // Part 18 - give reputation tokens
+      await fetch(`https://testnet.mirrornode.hedera.com/api/v1/accounts/${customer}`)
+    .then((response) => response.json())
+    .then(async (data) => {
+     console.log(data.evm_address);
+     const tx = await contract.scoring(data.evm_address, score, {
+      gasLimit: 1_000_000,
+     });
+     await tx.wait();
+    });
 
       // Part 19 - submit give REP tokens logs to topic
+      new TopicMessageSubmitTransaction()
+    .setTopicId(topicId)
+    .setMessage(
+     `{
+   type: Scoring,
+   accountAddr: ${customer},
+   tokenId: ${ftId.toString()},
+   amount: ${1}
+  }`
+    )
+    .execute(client);
 
       alert("Successfully gave REP tokens!");
     } catch (e) {
